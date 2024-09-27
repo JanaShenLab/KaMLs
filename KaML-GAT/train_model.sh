779322fcab83b79881623077d1398f0815f155a8
MingzheShen <57078708+MingzheShen@users.noreply.github.com>
2024-09-27 12:08:00 -0400

    Update train_model.sh

--- KaML-GAT/train_model.sh
+++ KaML-GAT/train_model.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
-#usage bash train_model.sh model_name n_expt
 
-model="$1"
 for n_expt in {10..19};
 do
 for fold in {0..9};
 do
-sed "s/%n_expt%/$n_expt/g" $model > tmp.py
+sed "s/%n_expt%/$n_expt/g" train_model.py > tmp.py
 sed "s/%n_fold%/$fold/g" tmp.py > tmp2.py
 python tmp2.py
+rm tmp.py tmp2.py
 done
 done
